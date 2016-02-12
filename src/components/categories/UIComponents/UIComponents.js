720d60716d18bc4cd7ceb54b79d186dec8f60e77
Juan Pablo Lomeli Diaz <tinkertrain@gmail.com>
2016-02-12 17:08:18 -0600

    Adds UI Components component

---
+++ src/components/categories/UIComponents/UIComponents.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export class UIComponents extends Component {
+  render() {
+    return (
+      <div>
+        <h3>UI Components!</h3>
+      </div>
+    );
+  }
+}
