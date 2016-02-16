c500949185ed7691eb9ccb3ca98ba4454173f05c
Juan Pablo Lomeli Diaz <tinkertrain@gmail.com>
2016-02-16 17:07:56 -0600

    loads css files without modules

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -36,7 +36,7 @@ module.exports = {
         test: /\.css$/,
         loaders: [
           'style-loader',
-          'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'
+          'css-loader'
         ]
       },
       {
