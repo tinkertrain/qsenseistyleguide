52f1b0e02ec608cf683a1a6ffc6a24490edad5da
Juan Pablo Lomeli Diaz <tinkertrain@gmail.com>
2016-02-16 17:10:24 -0600

    Design Elements component updated

--- src/components/categories/DesignElements/DesignElements.js
+++ src/components/categories/DesignElements/DesignElements.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-
+import styles from './DesignElements.scss';
 
 export class DesignElements extends Component {
   render() {
+
+    const { location } = this.props;
+    const currentPathArray = location.pathname.split('/');
+    const currentPath = currentPathArray[currentPathArray.length - 1];
+
     return (
       <div>
-        <h2>Design Elements!</h2>
-        {this.props.children || (
-          <p>Design Elements description could go here, or not</p>
-        )}
+        <header className={styles.header}>
+          <h4>Design Elements: {currentPath}</h4>
+        </header>
+        <section className={styles.main}>
+          {this.props.children || (
+            <p>Design Elements description could go here, or not</p>
+          )}
+        </section>
       </div>
     );
   }
