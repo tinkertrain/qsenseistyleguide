78a6e994fd77b288430f7ca7f974170088542920
Juan Pablo Lomeli Diaz <tinkertrain@gmail.com>
2016-02-17 17:08:43 -0600

    Makes small adjustments

--- src/components/Index/Index.js
+++ src/components/Index/Index.js
@@ -4,7 +4,7 @@ export class Index extends Component {
   render() {
     return (
       <div>
-        <h1>Sidebar</h1>
+        <h1>Website Style Guide</h1>
         <p>
           Routes can have multiple components, so that all portions of your UI
           can participate in the routing.
