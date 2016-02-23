96933d1a30db86e5e75bf736c9afdfe5115a1715
Juan Pablo Lomeli Diaz <tinkertrain@gmail.com>
2016-02-23 15:09:49 -0600

    Renames component Index to Home

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  render() {
+    return (
+      <div>
+        <section className="bgLight pageSection">
+          <h1>Style Guide</h1>
+          <p>
+            Q-Sensei's website style guide.
+          </p>
+        </section>
+      </div>
+    );
+  }
+}
