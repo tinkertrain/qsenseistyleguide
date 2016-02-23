5519bfc888b1c66a1d78845b5da3e39f03124dc2
Juan Pablo Lomeli Diaz <tinkertrain@gmail.com>
2016-02-23 15:11:11 -0600

    Uses IndexLink from react router

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import { IndexLink } from 'react-router';
 import logo from '../../images/logo_white.png';
 
 require('./Header.scss');
@@ -8,9 +8,9 @@ export class Header extends Component {
   render() {
     return (
       <header className="mainHeader">
-        <Link to="/">
+        <IndexLink to="/">
           <img src={logo} alt="Q-Sensei"/>
-        </Link>
+        </IndexLink>
       </header>
     );
   }
