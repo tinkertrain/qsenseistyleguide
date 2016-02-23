10a3203ee627daec1842aace4d590836a2c14c5d
Juan Pablo Lomeli Diaz <tinkertrain@gmail.com>
2016-02-23 15:12:37 -0600

    Splits routes into own module for both dev and prod usage

--- src/index.js
+++ src/index.js
@@ -1,34 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { browserHistory, Router, Route } from 'react-router';
+import { renderToString } from 'react-dom/server';
+import { createMemoryHistory, browserHistory, Router, Route, IndexRoute, RouterContext, match } from 'react-router';
+import template from './base-html';
 
-import { App } from './App';
+import routes from './routes';
 
-import Container from './components/categories/Container';
-import Colors from './components/categories/DesignElements/Colors/';
-import Typography from './components/categories/DesignElements/Typography/';
-import Buttons from './components/categories/UIComponents/Buttons/';
-import FormElements from './components/categories/UIComponents/FormElements/';
-import Code from './components/categories/UIComponents/Code/';
+if (typeof document !== 'undefined') {
+  render(
+    <Router routes={routes} history={browserHistory} />,
+    document.getElementById('root'));
+}
 
-render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <Route
-        path="design_elements"
-        component={Container}
-      >
-        <Route path="colors" component={Colors} />
-        <Route path="typography" component={Typography} />
-      </Route>
-      <Route
-        path="ui_components"
-        component={Container}
-        >
-        <Route path="buttons" component={Buttons} />
-        <Route path="form_elements" component={FormElements} />
-        <Route path="code" component={Code} />
-      </Route>
-    </Route>
-  </Router>
-), document.getElementById('root'));
+export default (locals, callback) => {
+  const history = createMemoryHistory();
+  const location = history.createLocation(locals.path);
+
+  match({ routes, location }, (error, redirectLocation, renderProps) => {
+    callback(
+      null,
+      template(renderToString(<RouterContext {...renderProps} />), locals.path, locals.assets)
+    );
+  });
+};
---
+++ src/routes.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import { App } from './App';
+import { Home } from './components/Home/Home';
+import Container from './components/categories/Container';
+
+import Colors from './components/categories/DesignElements/Colors/';
+import Typography from './components/categories/DesignElements/Typography/';
+
+import Buttons from './components/categories/UIComponents/Buttons/';
+import FormElements from './components/categories/UIComponents/FormElements/';
+import Code from './components/categories/UIComponents/Code/';
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={Home}/>
+
+    <Route
+      path="design_elements"
+      component={Container}
+      >
+      <Route path="colors" component={Colors} />
+      <Route path="typography" component={Typography} />
+    </Route>
+
+    <Route
+      path="ui_components"
+      component={Container}
+      >
+      <Route path="buttons" component={Buttons} />
+      <Route path="form_elements" component={FormElements} />
+      <Route path="code" component={Code} />
+    </Route>
+  </Route>
+);
