374f5884d7b930ddd88599be791b350f413b5412
Juan Pablo Lomeli Diaz <tinkertrain@gmail.com>
2016-03-01 11:09:14 -0600

    Adds debounce function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -12,3 +12,27 @@ export function capitalizePath(path) {
 
   return processArr.join(' ').trim();
 }
+
+export function debounce(func, wait, immediate) {
+  let timeout;
+
+  return function() {
+    let context = this, args = arguments;
+
+    var later = function() {
+      timeout = null;
+
+      if (!immediate) {
+        func.apply(context, args);
+      }
+    };
+
+    let callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+
+    if (callNow) {
+      func.apply(context, args);
+    }
+  };
+}
