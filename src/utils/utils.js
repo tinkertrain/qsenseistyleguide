8ef94b59c6991af75d3144bb6771feeb36286e47
Juan Pablo Lomeli Diaz <tinkertrain@gmail.com>
2016-03-08 18:09:06 -0600

    Adds throttle function and custom debounce fn

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -13,26 +13,59 @@ export function capitalizePath(path) {
   return processArr.join(' ').trim();
 }
 
-export function debounce(func, wait, immediate) {
-  let timeout;
+export function debounce(fn, wait) {
+  // Each call to the returned function will share this common timer.
+  var timeout;
 
+  // Calling debounce returns a new anonymous function
   return function() {
-    let context = this, args = arguments;
+    // reference the context and args for the setTimeout function
+    var args = arguments;
 
-    var later = function() {
+    // This is the basic debounce behaviour where you can call this
+    //   function several times, but it will only execute once
+    //   Each time the returned function is called, the timer starts over.
+    clearTimeout(timeout);
+
+    // Set the new timeout
+    timeout = setTimeout(() => {
+      // Inside the timeout function, clear the timeout variable
       timeout = null;
 
-      if (!immediate) {
-        func.apply(context, args);
-      }
-    };
+      // Call the original function with apply
+      fn.apply(this, args);
+    }, wait);
+  };
+}
 
-    let callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
 
-    if (callNow) {
-      func.apply(context, args);
+export function throttle(fn, threshhold = 100) {
+  // Each call to the returned function will share this common timer.
+  var deferTimer;
+  // Store when it was last called
+  var last;
+
+  return function() {
+    var now = +(new Date);
+    var args = arguments;
+
+    // Check if it was called (last would have a value) and
+    // if now is less than that time when it was called plus the threshhold
+    // if it is, it means the set time (threshhold) has passed and we can call
+    // the function again
+    if (last && (now < last + threshhold)) {
+      // Restart the timer
+      clearTimeout(deferTimer);
+
+      deferTimer = setTimeout(() => {
+        last = now;
+        fn.apply(this, args);
+      }, threshhold);
+    }
+    // Call the function for the first time
+    else {
+      last = now;
+      fn.apply(this, args);
     }
   };
 }
